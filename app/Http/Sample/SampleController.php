1eec955d17e7a4182e5d52b9845d98c8fbdaaec9
wowpowhub <wowpowhub@gmail.com>
2021-07-08 10:52:40 +0200

    Controller example

---
+++ app/Http/Sample/SampleController.php
@@ -0,0 +1,122 @@
+<?php
+namespace App\Http\Sample;
+
+use Exception;
+use Furo\Response;
+use App\Model\Sample\SampleModel;
+
+/**
+ * SampleController controller class
+ */
+class SampleController
+{
+	function GetId()
+	{
+		$ex = null;
+		$msg = '';
+
+		try
+		{
+			// Middleware (authenticate user first)
+			$user = $_SESSION['user'];
+			// Model
+			$o = new SampleModel();
+			$msg = $o->get($user->id);
+		}
+		catch (Exception $e) {
+			$ex = $e;
+			$msg = 'invalid_id';
+		}
+
+		return Response::httpError($ex)::jsonStatus([
+			'res' => [
+				'profil' => $msg
+			]
+		]);
+	}
+
+	function Get()
+	{
+		$ex = null;
+		$msg = '';
+
+		try
+		{
+			// Middleware
+			$user = $_SESSION['user'];
+			// Model
+			$o = new SampleModel();
+			$o->limit($_POST['limit'],$_POST['offset']);
+			$o->search($_POST['search']);
+			$msg = $o->all();
+		}
+		catch (Exception $e) {
+			$ex = $e;
+			$msg = null;
+		}
+
+		return Response::httpError($ex)::jsonStatus([
+			'res' => [
+				'profil' => $msg
+			]
+		]);
+	}
+
+	function Update()
+	{
+		$ex = null;
+		$msg = 'updated';
+
+		try
+		{
+			// Middleware
+			$user = $_SESSION['user'];
+			// Model
+			$o = new SampleModel();
+			// Validate variables with model class
+			foreach ($_POST as $k => $v) {
+				$o->$k = $v;
+			}
+			// Db
+			$o->update($_POST, $user->id);
+		}
+		catch (Exception $e) {
+			$ex = $e;
+			$msg = 'update_error';
+		}
+
+		return Response::httpError($ex)::jsonStatus([
+			'res' => [
+				'profil' => $msg
+			]
+		]);
+	}
+
+	function Delete()
+	{
+		$ex = null;
+		$msg = 'deleted';
+
+		try
+		{
+			// Middleware
+			$user = $_SESSION['user'];
+			// Validate
+			if($user->role != 'admin' && $user->role != 'worker') {
+				// Model
+				$o = new SampleModel();
+				$msg = $o->delete($user->id);
+			}
+		}
+		catch (Exception $e) {
+			$ex = $e;
+			$msg = 'not_deleted';
+		}
+
+		return Response::httpError($ex)::jsonStatus([
+			'res' => [
+				'profil' => $msg
+			]
+		]);
+	}
+}
